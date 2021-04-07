ac174ab0d8854d9166173033f881650baa068ca4
Arad Tenenbaum <arad.tenen@gmail.com>
2021-04-07 20:37:56 +0300

    init server

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, {Application, Request, Response} from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname.replace('src', '')+'.env' });
+const cors = require('cors');
+
+
+const PORT: Number = parseInt(<string>process.env.PORT, 10) || 5000;
+const DB_CONNECTION: string = (<string>process.env.DB_CONNECTION).toString();
+
+const app: Application = express();
+app.use(express.json());
+
+app.use(cors());
+
+// Routes
+
+// Connect DB
+mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {
+        console.log('DB Connected');
+        app.listen(PORT, () => {
+            console.log('Server is running');
+        });
+    });
+
+// app.get('/', (req: Request, res: Response) => res.send('Hello'));
